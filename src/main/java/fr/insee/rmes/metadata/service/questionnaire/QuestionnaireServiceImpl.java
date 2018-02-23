daa48a4ce09291c3adcd4db03fc15930335ad771
{Pierre Debuisson} <{pierre.debuisson@insee.fr}>
2018-02-23 16:59:30 +0100

    refactoring comments for getQuestionnaire

--- src/main/java/fr/insee/rmes/metadata/service/questionnaire/QuestionnaireServiceImpl.java
+++ src/main/java/fr/insee/rmes/metadata/service/questionnaire/QuestionnaireServiceImpl.java
@@ -35,29 +35,41 @@ public class QuestionnaireServiceImpl implements QuestionnaireService {
 	XpathProcessor xpathProcessor;
 
 	@Override
-	public String getQuestionnaire(String idDdiInstance, String idDdiInstrument) throws Exception {
+	public String getQuestionnaire(String idDDIInstance, String idDDIInstrument) throws Exception {
 
-		// Step 1 : Get the DdiItem and Check if it's a DDI instance (an
+		// Step 1 : Get the DDIInstance, the DDIInstrument and Check type (an
 		// Exception throws if not)
+		//////////////////////////////////////////////
+		// While Instrument not found
+		// Step 2 : Get all the group references
 
-		// Step 2 : Get the ColecticaItem group and the associated subGroup
-		// References
+		// Step 3 : foreach group in groups --> Search subGroups and store the
+		// currentGroup as ColecticaItem
 
-		// Step 3 : Get the ColecticaItem sub-group and the associated studyUnit
-		// References
+		// Step 4 : foreach subGroup in subGroups in currentGroup --> Search
+		// StudyUnits and store the currentSubGroup as ColecticaItem
 
-		// Step 4 : Get the ColecticaItem studyUnit and all its children
+		// Step 5 : foreach StudyUnit in currentStudyUnit in currentSubGroup in
+		// currentGroup -->
+		// Search Instruments and store the currentStudyUnit as ColecticaItem
 
-		// Step 5 : Among all of the studyUnit's chidren, search the instrument
+		// Step 6 : foreach Instrument in instruments in currentStudyUnit in
+		// currentSubGroup in currentGroup --> Search idDDIInstrument
+
+		//// Step 7 : if the idDDIInstrument : leave the Loop and get the
+		//// children list of this instrument
+		/////////////////////////////////////////////////////////////////
+
+		// Step 8 : Among all of the studyUnit's chidren, search the instrument
 		// relating to its id as parameter
 
-		// Step 6 : Insert the content of the instrument got to the enveloppe as
+		// Step 9 : Insert the content of the instrument got to the enveloppe as
 		// a child of StudyUnit's template.
 
-		// Step 7 : Insert the other references of the studyUnit to the
+		// Step 10 : Insert the other references of the studyUnit to the
 		// enveloppe as children of RessourcePackageTemplate
 
-		// Step 8 : return the filled out enveloppe as result
+		// Step 11 : return the filled out enveloppe as result
 
 		return "";
 	}
