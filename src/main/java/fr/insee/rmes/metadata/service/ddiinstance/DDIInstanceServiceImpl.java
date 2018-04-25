4c0c1262a1c2393a43b55a3b78d26be2f70d853c
{Pierre Debuisson} <{pierre.debuisson@insee.fr}>
2018-04-25 13:49:45 +0200

    fix a bug when the second ressource package is missing in the DDIInstance

--- src/main/java/fr/insee/rmes/metadata/service/ddiinstance/DDIInstanceServiceImpl.java
+++ src/main/java/fr/insee/rmes/metadata/service/ddiinstance/DDIInstanceServiceImpl.java
@@ -38,45 +38,59 @@ public class DDIInstanceServiceImpl implements DDIInstanceService {
 
 	@Autowired
 	XpathProcessor xpathProcessor;
-	
+
 	@Override
-	public String getDDIInstance(String id) throws Exception {		
-		
-		
-		// Step 1 : Get the ColecticaItem and Check if it's a DDI instance (an Exception throws if not)
-		ColecticaItem ddiInstance = metadataServiceItem.getDDIInstance(id); 
-		// Step 2 : Get the ColecticaItem group 
-		String idGroup = xpathProcessor.queryString(ddiInstance.getItem(), "/Fragment[1]/DDIInstance[1]/GroupReference[1]/ID[1]/text()");
-		ColecticaItem groupItem = metadataServiceItem.getItem(idGroup); 
+	public String getDDIInstance(String id) throws Exception {
+
+		// Step 1 : Get the ColecticaItem and Check if it's a DDI instance (an
+		// Exception throws if not)
+		ColecticaItem ddiInstance = metadataServiceItem.getDDIInstance(id);
+		// Step 2 : Get the ColecticaItem group
+		String idGroup = xpathProcessor.queryString(ddiInstance.getItem(),
+				"/Fragment[1]/DDIInstance[1]/GroupReference[1]/ID[1]/text()");
+		ColecticaItem groupItem = metadataServiceItem.getItem(idGroup);
 		// Step 3 : Get the ColecticaItem sub-group
-		String idSubGroup = xpathProcessor.queryString(groupItem.getItem(), "/Fragment[1]/Group[1]/SubGroupReference[1]/ID[1]/text()");
-		ColecticaItem subGroupItem = metadataServiceItem.getItem(idSubGroup); 
+		String idSubGroup = xpathProcessor.queryString(groupItem.getItem(),
+				"/Fragment[1]/Group[1]/SubGroupReference[1]/ID[1]/text()");
+		ColecticaItem subGroupItem = metadataServiceItem.getItem(idSubGroup);
 		// Step 4 : Get the ColecticaItem Study-unit
-		String idStudyUnit = xpathProcessor.queryString(subGroupItem.getItem(), "/Fragment[1]/SubGroup[1]/StudyUnitReference[1]/ID[1]/text()");
-		ColecticaItem studyUnitItem = metadataServiceItem.getItem(idStudyUnit); 
+		String idStudyUnit = xpathProcessor.queryString(subGroupItem.getItem(),
+				"/Fragment[1]/SubGroup[1]/StudyUnitReference[1]/ID[1]/text()");
+		ColecticaItem studyUnitItem = metadataServiceItem.getItem(idStudyUnit);
 		// Step 5 : Get the ColecticaItem PhysicalDataProduct
-		String idPhysicalDataProduct = xpathProcessor.queryString(subGroupItem.getItem(), "/Fragment[1]/SubGroup[1]/PhysicalDataProductReference[1]/ID[1]/text()");
-		ColecticaItem physicalDataProductItem = metadataServiceItem.getItem(idPhysicalDataProduct); 
+		String idPhysicalDataProduct = xpathProcessor.queryString(subGroupItem.getItem(),
+				"/Fragment[1]/SubGroup[1]/PhysicalDataProductReference[1]/ID[1]/text()");
+		ColecticaItem physicalDataProductItem = metadataServiceItem.getItem(idPhysicalDataProduct);
 		// Step : Build the group, from the studyUnit to the group
 		DDIDocumentBuilder docBuilder = new DDIDocumentBuilder();
-		
-		Node subGroupNode = getNode(UtilXML.nodeToString(xpathProcessor.queryList(subGroupItem.getItem(),"/Fragment[1]/*").item(0)),docBuilder.getDocument());
+
+		Node subGroupNode = getNode(
+				UtilXML.nodeToString(xpathProcessor.queryList(subGroupItem.getItem(), "/Fragment[1]/*").item(0)),
+				docBuilder.getDocument());
 		NodeList subGroupChildrenNodes = subGroupNode.getChildNodes();
 		for (int i = 0; i < subGroupChildrenNodes.getLength(); i++) {
 			Node node = subGroupChildrenNodes.item(i);
 			if (node.getNodeName().contains("StudyUnitReference")) {
-				Node studyUnitNode = getNode(UtilXML.nodeToString(xpathProcessor.queryList(studyUnitItem.getItem(),"/Fragment[1]/*").item(0)),docBuilder.getDocument());
+				Node studyUnitNode = getNode(
+						UtilXML.nodeToString(
+								xpathProcessor.queryList(studyUnitItem.getItem(), "/Fragment[1]/*").item(0)),
+						docBuilder.getDocument());
 				subGroupNode.removeChild(node);
 				subGroupNode.appendChild(studyUnitNode);
 			}
 			if (node.getNodeName().contains("PhysicalDataProductReference")) {
-				Node physicalDataProductNode = getNode(UtilXML.nodeToString(xpathProcessor.queryList(physicalDataProductItem.getItem(),"/Fragment[1]/*").item(0)),docBuilder.getDocument());
+				Node physicalDataProductNode = getNode(
+						UtilXML.nodeToString(
+								xpathProcessor.queryList(physicalDataProductItem.getItem(), "/Fragment[1]/*").item(0)),
+						docBuilder.getDocument());
 				subGroupNode.removeChild(node);
 				subGroupNode.appendChild(physicalDataProductNode);
 			}
 		}
-		subGroupNode = getNode(UtilXML.nodeToString(subGroupNode),docBuilder.getDocument());
-		Node groupNode = getNode(UtilXML.nodeToString(xpathProcessor.queryList(groupItem.getItem(),"/Fragment[1]/*").item(0)),docBuilder.getDocument());
+		subGroupNode = getNode(UtilXML.nodeToString(subGroupNode), docBuilder.getDocument());
+		Node groupNode = getNode(
+				UtilXML.nodeToString(xpathProcessor.queryList(groupItem.getItem(), "/Fragment[1]/*").item(0)),
+				docBuilder.getDocument());
 		NodeList groupChildrenNodes = groupNode.getChildNodes();
 		for (int i = 0; i < groupChildrenNodes.getLength(); i++) {
 			Node node = groupChildrenNodes.item(i);
@@ -85,29 +99,41 @@ public class DDIInstanceServiceImpl implements DDIInstanceService {
 				groupNode.removeChild(node);
 			}
 		}
-		
-		// Step  : Get the first Resource package
-		String idRP = xpathProcessor.queryString(ddiInstance.getItem(), "/Fragment[1]/DDIInstance[1]/ResourcePackageReference[1]/ID[1]/text()");
-		String rpString = xpathProcessor.queryString(metadataService.getDerefDDIDocument(idRP),"/DDIInstance[1]/*");
-		Node RP1 = getNode(rpString,docBuilder.getDocument());
-		// Step  : Get the second Resource package
-		String idRP2 = xpathProcessor.queryString(ddiInstance.getItem(), "/Fragment[1]/DDIInstance[1]/ResourcePackageReference[2]/ID[1]/text()");
-		String rpString2 = xpathProcessor.queryString(metadataService.getDerefDDIDocument(idRP2),"/DDIInstance[1]/*");
-		Node RP2 = getNode(rpString2,docBuilder.getDocument());
-		
-		// Step : Get DDI Instance informations on root : r:URN, r:Agency, r:ID, r:Version, r:UserID, r:Citation
+
+		// Step : Get the first Resource package
+		String idRP = xpathProcessor.queryString(ddiInstance.getItem(),
+				"/Fragment[1]/DDIInstance[1]/ResourcePackageReference[1]/ID[1]/text()");
+		String rpString = xpathProcessor.queryString(metadataService.getDerefDDIDocument(idRP), "/DDIInstance[1]/*");
+		Node RP1 = getNode(rpString, docBuilder.getDocument());
+		// Step : Get the second Resource package (if available)
+		Node RP2 = null;
+		try {
+			String idRP2 = xpathProcessor.queryString(ddiInstance.getItem(),
+					"/Fragment[1]/DDIInstance[1]/ResourcePackageReference[2]/ID[1]/text()");
+			String rpString2 = xpathProcessor.queryString(metadataService.getDerefDDIDocument(idRP2),
+					"/DDIInstance[1]/*");
+			RP2 = getNode(rpString2, docBuilder.getDocument());
+		} catch (Exception e) {
+		}
+
+		// Step : Get DDI Instance informations on root : r:URN, r:Agency, r:ID,
+		// r:Version, r:UserID, r:Citation
 		String urnString = xpathProcessor.queryString(ddiInstance.getItem(), "/Fragment[1]/DDIInstance[1]/URN[1]");
-		Node urnNode = getNode(urnString.trim(),docBuilder.getDocument());
-		String agencyString = xpathProcessor.queryString(ddiInstance.getItem(), "/Fragment[1]/DDIInstance[1]/Agency[1]");
-		Node agencyNode = getNode(agencyString,docBuilder.getDocument());
+		Node urnNode = getNode(urnString.trim(), docBuilder.getDocument());
+		String agencyString = xpathProcessor.queryString(ddiInstance.getItem(),
+				"/Fragment[1]/DDIInstance[1]/Agency[1]");
+		Node agencyNode = getNode(agencyString, docBuilder.getDocument());
 		String idString = xpathProcessor.queryString(ddiInstance.getItem(), "/Fragment[1]/DDIInstance[1]/ID[1]");
-		Node idNode = getNode(idString,docBuilder.getDocument());
-		String versionString = xpathProcessor.queryString(ddiInstance.getItem(), "/Fragment[1]/DDIInstance[1]/Version[1]");
-		Node versionNode = getNode(versionString,docBuilder.getDocument());
-		String userIDString = xpathProcessor.queryString(ddiInstance.getItem(), "/Fragment[1]/DDIInstance[1]/UserID[1]");
-		Node userIDNode = getNode(userIDString,docBuilder.getDocument());
-		String citationString = xpathProcessor.queryString(ddiInstance.getItem(), "/Fragment[1]/DDIInstance[1]/Citation[1]");
-		Node citationNode = getNode(citationString,docBuilder.getDocument());
+		Node idNode = getNode(idString, docBuilder.getDocument());
+		String versionString = xpathProcessor.queryString(ddiInstance.getItem(),
+				"/Fragment[1]/DDIInstance[1]/Version[1]");
+		Node versionNode = getNode(versionString, docBuilder.getDocument());
+		String userIDString = xpathProcessor.queryString(ddiInstance.getItem(),
+				"/Fragment[1]/DDIInstance[1]/UserID[1]");
+		Node userIDNode = getNode(userIDString, docBuilder.getDocument());
+		String citationString = xpathProcessor.queryString(ddiInstance.getItem(),
+				"/Fragment[1]/DDIInstance[1]/Citation[1]");
+		Node citationNode = getNode(citationString, docBuilder.getDocument());
 
 		// Final step add Child on root
 		docBuilder.appendChild(urnNode);
@@ -118,11 +144,13 @@ public class DDIInstanceServiceImpl implements DDIInstanceService {
 		docBuilder.appendChild(citationNode);
 		docBuilder.appendChild(groupNode);
 		docBuilder.appendChild(RP1);
-		docBuilder.appendChild(RP2);
+		if (RP2 != null) {
+			docBuilder.appendChild(RP2);
+		}
 		return docBuilder.toString();
-		
+
 	}
-	
+
 	private Document getDocument(String fragment) throws Exception {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
@@ -132,7 +160,7 @@ public class DDIInstanceServiceImpl implements DDIInstanceService {
 		InputSource ddiSource = new InputSource(new StringReader(fragment));
 		return builder.parse(ddiSource);
 	}
-	
+
 	private Node getNode(String fragment, Document doc) throws Exception {
 		Element node = getDocument(fragment).getDocumentElement();
 		Node newNode = node.cloneNode(true);
